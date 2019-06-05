6c39f0d23b8146bc0c154f8399c7482bb531f478
Reyes1234 <50181399+Reyes1234@users.noreply.github.com>
2019-06-05 14:49:34 -0400

    My Character
    Updated

--- Characters.js
+++ Characters.js
@@ -8,11 +8,11 @@
          return this.name +" " + this.favoritesong;
        }
       }
-    }
+
 
 let myCharacter = new Character("Tim","17", "We Are Young");
 let anotherCharacter = new Character("Ronda","28", "My Heart Will Go On");
-let anotherCharacter = new Character("Emma","21","Thriller");
-let anotherCharacter = new Character("James","25","Teardrops on my Guitar");
+let thirdCharacter = new Character("Emma","21","Thriller");
+let fourthCharacter = new Character("James","25","Teardrops on my Guitar");
 
 console.log(myCharacter);
